aadd2ce96936e5131f719828d85593fc10314898
Minh Tran <wisekingtran@gmail.com>
2021-03-15 23:10:26 +0700

    Support S3 Object Storage

--- src/main/java/vn/minhtran/study/service/impl/DefaultMediaService.java
+++ src/main/java/vn/minhtran/study/service/impl/DefaultMediaService.java
@@ -1,17 +1,22 @@
 package vn.minhtran.study.service.impl;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.HeadBucketRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.waiters.WaiterParameters;
 
 import vn.minhtran.study.infra.config.LocalStorageProperties;
 import vn.minhtran.study.infra.persistence.entity.AlbumEntity;
@@ -26,6 +31,21 @@ public class DefaultMediaService extends AbstractGooglePhoto
 
 	@Autowired
 	private LocalStorageProperties storageProperties;
+	
+	@Autowired
+	private AmazonS3Client s3Client;
+	
+	@PostConstruct
+	void init() {
+		s3Client
+		  .waiters()
+		  .bucketExists()
+		  .run(
+		    new WaiterParameters<>(
+		      new HeadBucketRequest("my-awesome-bucket")
+		    )
+		  );
+	}
 
 	@Override
 	public void downloadPhoto(String baseUrl, String albumName, String width,
@@ -42,10 +62,15 @@ public class DefaultMediaService extends AbstractGooglePhoto
 					if (!ret.exists()) {
 						ret.createNewFile();
 					}
+//					try (InputStream is = clientHttpResponse.getBody()) {
+//						try (FileOutputStream os = new FileOutputStream(ret)) {
+//							StreamUtils.copy(is, os);
+//						}
+//					}
+					
 					try (InputStream is = clientHttpResponse.getBody()) {
-						try (FileOutputStream os = new FileOutputStream(ret)) {
-							StreamUtils.copy(is, os);
-						}
+						ObjectMetadata metadata=new ObjectMetadata();
+						s3Client.putObject("my-awesome-bucket", filename, is, metadata);
 					}
 					return ret;
 				});
