c000f909f6489e1584f24519b8ec36a7f900636b
Minh Tran <wisekingtran@gmail.com>
2021-03-28 11:31:09 +0700

    Media download

--- src/main/java/vn/minhtran/study/controller/MediaController.java
+++ src/main/java/vn/minhtran/study/controller/MediaController.java
@@ -1,5 +1,17 @@
 package vn.minhtran.study.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.minhtran.study.model.MediaDownloadResponse;
+
+@RestController
+@RequestMapping("/media")
 public class MediaController {
 
+	@GetMapping("/download")
+	public MediaDownloadResponse download() {
+
+	}
 }
